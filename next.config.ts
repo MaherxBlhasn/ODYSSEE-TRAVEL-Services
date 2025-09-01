bb8031b67cac0989fe0979b211304b724a31e42e
maher <mahertun5@gmail.com>
2025-09-01 22:21:48 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default withNextIntl(nextConfig);
+const nextConfig = {
+    async redirects() {
+        return [];
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/sitemap',
+                destination: '/sitemap',
+                locale: false, // <- important: prevent locale prefix
+            },
+        ];
+    },
+};
+export default nextConfig;

fc8299f82a0e01906d3344c86c6f29e394eaec4b
maher <mahertun5@gmail.com>
2025-09-01 23:24:21 +0100

    addding robots

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,7 @@ export default createMiddleware({
 
 export const config = {
   matcher: [
-    // Match everything except API, Next.js static files, sitemap, webmanifest, favicon, images, etc.
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap|site\\.webmanifest|.*\\.(?:png|jpg|jpeg|gif|svg|webp|ico)).*)',
+    // Match everything except API, Next.js internals, sitemap, robots, webmanifest, favicon, assets, etc.
+    '/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap|site\\.webmanifest|.*\\.(?:png|jpg|jpeg|gif|svg|webp|ico)).*)',
   ],
 };
